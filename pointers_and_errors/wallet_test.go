a5cc8d23f8445b7f9bb334361c384bc21430e910
kamah-01 <millerbeautyagencies@gmail.com>
2024-06-16 19:23:47 +0300

    pointers and error

---
+++ pointers_and_errors/wallet_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+func TestWallet(t *testing.T) {
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		if !reflect.DeepEqual(got,want)   {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		if !reflect.DeepEqual(got,want){
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+	t.Run("withdrawing insufficient funds",func(t *testing.T) {
+		startingBalance:=Bitcoin(20)
+		wallet:=Wallet{startingBalance}
+		err:=wallet.Withdraw(Bitcoin(100))
+		if err == nil {
+			t.Errorf("Wanted an error but didn't give one")
+		}
+	})
+}
